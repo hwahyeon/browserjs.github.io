d11eb3e30d3224dc38fb3c02a5385a7f9f37baeb
Janghyeon <hwahyeon@knou.ac.kr>
2024-07-15 23:55:45 +0200

    Format code with formatter

--- js/greetings.js
+++ js/greetings.js
@@ -1,39 +1,40 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const greeting = document.querySelector("#greeting")
+const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
-    event.preventDefault();
-    loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, username);
-    paintGreeting(username);
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreeting(username);
 }
 
 const greetWorld = [
-    '안녕',
-    'Hi',
-    'Hello',
-    'Hola',
-    'Bonjour',
-    'Ola',
-    'Guten Tag',
-]
+  "안녕",
+  "Hi",
+  "Hello",
+  "Hola",
+  "Bonjour",
+  "Ola",
+  "Guten Tag",
+];
 
-function paintGreeting(username){
-    const greetWorldRandom = greetWorld[Math.floor(Math.random() * greetWorld.length)]
-    greeting.innerText = `${greetWorldRandom} ${username} !`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+function paintGreeting(username) {
+  const greetWorldRandom =
+    greetWorld[Math.floor(Math.random() * greetWorld.length)];
+  greeting.innerText = `${greetWorldRandom} ${username} !`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-if (savedUsername === null){
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener("submit", onLoginSubmit);
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-    paintGreeting(savedUsername);
+  paintGreeting(savedUsername);
 }
--- js/weather.js
+++ js/weather.js
@@ -1,23 +1,21 @@
-const API_KEY = "e407444da2473e51df65d8d6e214f4eb"
+const API_KEY = "e407444da2473e51df65d8d6e214f4eb";
 
-
-function onGeoOk(position){
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    fetch(url)
-    .then(response => response.json())
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
     .then((data) => {
-        const weather = document.querySelector("#weather span:first-child")
-        const city = document.querySelector("#weather span:last-child")
-        city.innerText = data.name;
-        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     });
 }
 
-function onGeoError(){
-    alert("Can't find you. No weather for you.");
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
-
